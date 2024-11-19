c42e8f85a35ce2c1c18141e15a343a21ba37eaa4
Christian Reinprecht <christian@Christians-MacBook-Pro.fritz.box>
2024-11-19 11:24:43 +0100

    Modifications for haptic guidacne

--- scripts/body_parts_pie.py
+++ scripts/body_parts_pie.py
@@ -4,11 +4,10 @@ import matplotlib.pyplot as plt
 # Load the data into a pandas DataFrame
 data = {
     'Paper': range(1, 37),
-    'Palm': [0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1],
-    'Fingers': [1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1],
-    'Feet': [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-    'Forearm': [0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0],
-    'Entire Arm': [1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1],
+    'Palm': [0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1],
+    'Fingers': [0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1],
+    'Forearm': [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0],
+    'Upper Arm': [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1],
     'Entire Body': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0]
 }
 
@@ -17,7 +16,7 @@ df = pd.DataFrame(data)
 plt.rcParams.update({
     "text.usetex": False,
     "font.family": "serif",
-    "font.serif": ['Times', 'Times New Roman', 'Liberation Serif'],  # Fallback options
+    "font.serif": ["Times"],  # or 'Times', 'Palatino', etc.
 })
 
 # Sum the occurrences of each body part
@@ -26,35 +25,23 @@ body_part_counts = df.drop(columns='Paper').sum()
 # Define colors for the pie chart
 colors = ['#ff9999','#66b3ff','#99ff99','#ffcc99','#c2c2f0','#ffb3e6']
 
+
 def autopct_generator(pct, allvalues):
     absolute = int(round(pct/100.*sum(allvalues)))
-    return '{:.1f}%\n({:d})'.format(pct, absolute)
-
-# Create a pie chart with improved spacing and no overlapping texts
+    return '{:.1f}%\n({:d})'.format(pct, absolute)# if pct > 5 else ''  # Display values only if larger than 5%
+# Create a pie chart with multiple levels
 fig, ax = plt.subplots()
 
-wedges, texts, autotexts = ax.pie(
-    body_part_counts,
-    labels=body_part_counts.index,
-    autopct=lambda pct: autopct_generator(pct, body_part_counts),
-    startangle=90,
-    counterclock=False,
-    colors=colors,
-    pctdistance=0.7,  # Adjusting this to avoid overlap
-    labeldistance=1.1  # Adjusting this to position labels further from the pie
-)
+wedges, texts, autotexts = ax.pie(body_part_counts, labels=body_part_counts.index, autopct=lambda pct: autopct_generator(pct, body_part_counts), startangle=90, counterclock=False, colors=colors)
 
-# Adjust font sizes and layout to avoid overlaps
 for text in texts:
-    text.set_fontsize(22)
+    text.set_fontsize(18)
+
 for autotext in autotexts:
-    autotext.set_fontsize(20)
+    autotext.set_fontsize(16)
 
 ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
-# Use tight_layout to optimize space and ensure nothing overlaps
-plt.tight_layout()
-
 # Display the pie chart
-plt.savefig('C:/Users/chris/Documents/Thesis/LiteratureResearch/figures/body_pie.pdf', format='pdf')
+plt.savefig('/Users/christian/Documents/Literature-Research-Master-Thesis/figures/body_pie.pdf', format='pdf')
 plt.show()
--- scripts/years.py
+++ scripts/years.py
@@ -3,40 +3,37 @@ import matplotlib
 import matplotlib.pyplot as plt
 from io import StringIO
 
-plt.rcParams.update({
-    "text.usetex": False,
-    "font.family": "serif",
-    "font.serif": ['Times', 'Times New Roman', 'Liberation Serif'],  # Fallback options
-})
+matplotlib.rcParams['mathtext.fontset'] = 'stix'
+matplotlib.rcParams['font.family'] = 'STIXGeneral'
 
 # Simulating reading the CSV file 'years.csv'
 # Assuming the file content is in the same format as the provided data
 csv_data = """Year,Amount
 2000,1
 2001,0
-2002,1
+2002,0
 2003,0
 2004,0
 2005,0
 2006,1
 2007,2
-2008,2
+2008,0
 2009,0
 2010,1
 2011,0
 2012,2
 2013,0
-2014,3
-2015,1
+2014,0
+2015,0
 2016,0
-2017,1
+2017,0
 2018,0
 2019,5
 2020,2
-2021,2
+2021,1
 2022,3
-2023,7
-2024,1"""
+2023,8
+2024,0"""
 
 # Read the data into a pandas DataFrame
 df = pd.read_csv(StringIO(csv_data))
@@ -54,7 +51,7 @@ df.reset_index(inplace=True)
 df['Year'] = df['Year'].dt.year
 
 # Plotting every year
-plt.figure(figsize=(2.3*1.2, 1.85*1.2))
+plt.figure(figsize=(4, 3))
 plt.bar(df['Year'].astype(str), df['Amount'], width=0.8)
 
 # Setting the x-axis labels to show only every 5 years
@@ -67,7 +64,7 @@ plt.ylabel('Number of studies')
 plt.title('')
 
 plt.tight_layout()  # Adjust layout
-plt.savefig("C:/Users/chris/Documents/Thesis/LiteratureResearch/figures/years.pdf", format="pdf", bbox_inches="tight")
+plt.savefig("/Users/christian/Documents/Literature-Research-Master-Thesis/figures/years.pdf", format="pdf", bbox_inches="tight")
 plt.show()
 
 
